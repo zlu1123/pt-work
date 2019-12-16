2ed38558a15ddee009826932723d5e7887749160
lukezhang <416632158@qq.com>
2019-12-16 20:29:07 +0800

    1.增加vue-cli3 webpack配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 module.exports = {
+  publicPath: './', // 打包后的位置(如果不设置这个静态资源会报404)
+  outputDir: 'dist', // 打包后的目录名称
+  assetsDir: 'static', // 静态资源目录名称
   pluginOptions: {
     'style-resources-loader': {
       preProcessor: 'less',
